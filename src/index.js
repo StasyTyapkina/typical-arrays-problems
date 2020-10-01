3de661df79bb73486ec05211f0a35268e0ed60be
StasyTyapkina <stasya.tyapkina@gmail.com>
2020-10-01 20:41:46 +0300

    feat: add basic mim max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array) && array.length != 0){
+    return Math.min.apply(Math,array);
+  }   
+    return 0; 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) && array.length != 0) {
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (max < array[i]) {
+          max = array[i];
+        } 
+    }
+    
+    return max;
+  } return 0;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array) && array.length != 0) {
+    var total = 0;
+    for (var i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    var avg = total / array.length;
+    
+    return avg;
+} return 0;
 }
